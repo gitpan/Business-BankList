cd10622553dc20901c492553843890ce3c13c20d
Steven Haryanto <stevenharyanto@gmail.com>
2013-04-25 01:53:00 +0000

    Import of SHARYANTO/Business-BankList-0.011 from CPAN.
    gitpan-cpan-distribution: Business-BankList
    gitpan-cpan-version:      0.011
    gitpan-cpan-path:         SHARYANTO/Business-BankList-0.011.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Business/BankList.pm
+++ lib/Business/BankList.pm
@@ -1,6 +1,6 @@
 package Business::BankList;
 
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.011'; # VERSION
 
 1;
 # ABSTRACT: List banks/financial institutions
@@ -15,7 +15,7 @@ Business::BankList - List banks/financial institutions
 
 =head1 VERSION
 
-version 0.01
+version 0.011
 
 =head1 SYNOPSIS
 
@@ -26,7 +26,7 @@ B<NOTE: This module is still empty without implementation.>
 This module provides a way to get a list of banks/financial institutions. The
 primary use is to get SWIFT code/BIC/IBAN or the like. This module is meant to
 be a "master" module. There will be submodules for each country, e.g.
-L<Finance::BankList::Indonesia>, and so on. Each country can provide additional
+L<Business::BankList::Indonesia>, and so on. Each country can provide additional
 information like country-specific codes.
 
 =head1 SEE ALSO
@@ -39,7 +39,7 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by Steven Haryanto.
+This software is copyright (c) 2013 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
